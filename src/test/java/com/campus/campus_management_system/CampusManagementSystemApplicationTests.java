9fbd0bad67fe3bc663a4f8ec3a3169b2c23f42cd
Borislav Hristov <test@example.com>
2026-03-10 15:41:52 +0200

    New test

--- src/test/java/com/campus/campus_management_system/CampusManagementSystemApplicationTests.java
+++ src/test/java/com/campus/campus_management_system/CampusManagementSystemApplicationTests.java
@@ -9,4 +9,10 @@ class CampusManagementSystemApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    void intentionalFailureTest() {
+        int result = 2 + 2;
+        Assertions.assertEquals(5, result, "Този тест умишлено гърми, за да тества CI/CD защитата!");
+    }
 }
