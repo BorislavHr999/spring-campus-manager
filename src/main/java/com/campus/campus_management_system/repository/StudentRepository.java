b87747e09f3e9d309ff6641cce8c47369bf9fe80
Borislav Hristov <test@example.com>
2026-03-11 13:14:41 +0200

    Fixed missing List import in StudentRepository

--- src/main/java/com/campus/campus_management_system/repository/StudentRepository.java
+++ src/main/java/com/campus/campus_management_system/repository/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.campus.campus_management_system.repository;
 
+import java.util.list;
 import com.campus.campus_management_system.model.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
